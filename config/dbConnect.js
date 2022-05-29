9f67bec37f791401b858cad1087aaa4545e12d31
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-29 12:07:05 +0100

    Updating mongoose DB connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,4 +1,29 @@
 import mongoose from 'mongoose';
-const dbConnect = () => {};
+
+const dbConnect = () => {
+	if (mongoose.connection.readyState >= 1) {
+		return;
+	}
+
+	mongoose.connect(process.env.DB_LOCAL_URI);
+	// .then(console.log('Connected to local database'));
+};
 
 export default dbConnect;
+
+// import mongoose from 'mongoose';
+
+// const dbConnect = () => {
+// 	if (mongoose.connection.readyState >= 1) {
+// 		return;
+// 	}
+
+// 	mongoose.connect(process.env.DB_LOCAL_URI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 		useFindAndModify: false,
+// 		UseCreateIndex: true,
+// 	});
+// };
+
+// export default dbConnect;
