4acf7baa64a0125c5f521071481a55216d87349b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-29 12:09:52 +0100

    Updating and cleaning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
+module.exports = {
+	env: {
+		DB_LOCAL_URI: 'mongodb://127.0.0.1/bookings-4-u',
+	},
+};
