4c8aa2713ee06544dd9b2873307ed938c707bdf8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-30 07:16:59 +0100

    Updating and switching from Compass to Atlas

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,29 +1,11 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const dbConnect = () => {
 	if (mongoose.connection.readyState >= 1) {
 		return;
 	}
 
-	mongoose.connect(process.env.DB_LOCAL_URI);
-	// .then(console.log('Connected to local database'));
+	mongoose.connect(process.env.DB_ATLAS_URI);
 };
 
 export default dbConnect;
-
-// import mongoose from 'mongoose';
-
-// const dbConnect = () => {
-// 	if (mongoose.connection.readyState >= 1) {
-// 		return;
-// 	}
-
-// 	mongoose.connect(process.env.DB_LOCAL_URI, {
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 		useFindAndModify: false,
-// 		UseCreateIndex: true,
-// 	});
-// };
-
-// export default dbConnect;
