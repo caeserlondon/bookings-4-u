331ae142e99b6930955bb0d9c8cfa818919dd35b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-30 21:57:26 +0100

    Adding the eslint comment

--- middlewares/catchAsyncErrors.js
+++ middlewares/catchAsyncErrors.js
@@ -1,4 +1,3 @@
-const catchAsyncErrors = (func) => (req, res, next) =>
+/* eslint-disable import/no-anonymous-default-export */
+export default (func) => (req, res, next) =>
 	Promise.resolve(func(req, res, next)).catch(next);
-
-export default catchAsyncErrors;
