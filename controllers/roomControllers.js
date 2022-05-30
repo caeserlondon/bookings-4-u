ec903b8f374fade78ccd04b58e8dabb5b5a61b27
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-30 18:36:43 +0100

    Creating Updating Room details functionality

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -57,4 +57,34 @@ const getSingleRoom = async (req, res) => {
 	}
 };
 
-export { allRooms, newRoom, getSingleRoom };
+// Update room details   =>   /api/rooms/:id
+const updateRoom = async (req, res) => {
+	try {
+		let room = await Room.findById(req.query.id);
+
+		if (!room) {
+			return res.status(404).json({
+				success: false,
+				error: 'Room not found with this ID',
+			});
+		}
+
+		room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+			new: true,
+			runValidators: true,
+			useFindAndModify: false,
+		});
+
+		res.status(200).json({
+			success: true,
+			room,
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+export { allRooms, newRoom, getSingleRoom, updateRoom };
