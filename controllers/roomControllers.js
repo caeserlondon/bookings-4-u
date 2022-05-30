8b4fbb9451ec716750de7ad8536f75852ed99418
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-30 05:50:04 +0100

    Adding Create newRoom function

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,7 +1,27 @@
+import Room from '../models/room';
+
 const allRooms = (req, res) => {
 	res.status(200).json({
 		success: true,
 		messege: 'all rooms',
 	});
 };
-export { allRooms };
+
+// Create new room   =>   /api/rooms
+const newRoom = async (req, res) => {
+	try {
+		const room = await Room.create(req.body);
+
+		res.status(200).json({
+			success: true,
+			room,
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+export { allRooms, newRoom };
