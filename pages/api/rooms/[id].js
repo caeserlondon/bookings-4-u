d4da2b37b71fccb46a114bf59c3e52802eb5a159
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-30 18:57:50 +0100

    Creating the dynamic route page

---
+++ pages/api/rooms/[id].js
@@ -0,0 +1,16 @@
+import nc from 'next-connect';
+import dbConnect from '../../../config/dbConnect';
+
+import {
+	getSingleRoom,
+	updateRoom,
+} from '../../../controllers/roomControllers';
+
+const handler = nc();
+
+dbConnect();
+
+handler.get(getSingleRoom);
+handler.put(updateRoom);
+
+export default handler;
