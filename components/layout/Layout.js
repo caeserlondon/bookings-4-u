bb7f444479f6914572ad402b357db5d7f02f704c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-01 18:33:00 +0100

    Updating the Layout Component in the layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
 
-const Layout = () => {
-	return <div>Layout</div>;
+const Layout = ({ children, title = 'BOOKINGS FOR YOU' }) => {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+
+			<Header />
+
+			{children}
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
