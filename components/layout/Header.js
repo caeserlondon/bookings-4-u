6a37ad446f9ce725173f2b65be393415062f6701
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-01 18:31:26 +0100

    Updating the Header Component in the layout

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<nav className="navbar row justify-content-center sticky-top">
+			<div className="container">
+				<div className="col-3 p-0">
+					<div className="navbar-brand">
+						<img src="images/globe.png" alt="Bookings-4-you" />
+					</div>
+				</div>
+
+				<div className="col-3 mt-3 mt-md-0 text-center">
+					<a className="btn btn-danger px-4 text-white login-header-btn float-right">
+						Login
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
