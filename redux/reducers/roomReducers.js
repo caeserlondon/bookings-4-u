94533e72b400b04e2f32938ef6d19e63c297025c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-01 18:26:12 +0100

    Creating the roomReducers for Redux

---
+++ redux/reducers/roomReducers.js
@@ -0,0 +1,33 @@
+import {
+	ALL_ROOMS_SUCCESS,
+	ALL_ROOMS_FAIL,
+	CLEAR_ERRORS,
+} from '../constants/roomConstants';
+
+// All rooms reducer
+export const allRoomsReducer = (state = { rooms: [] }, action) => {
+	switch (action.type) {
+		case ALL_ROOMS_SUCCESS:
+			return {
+				roomsCount: action.payload.roomsCount,
+				resPerPage: action.payload.resPerPage,
+				filteredRoomsCount: action.payload.filteredRoomsCount,
+				rooms: action.payload.rooms,
+			};
+
+		case ALL_ROOMS_FAIL:
+		case ADMIN_ROOMS_FAIL:
+			return {
+				error: action.payload,
+			};
+
+		case CLEAR_ERRORS:
+			return {
+				...state,
+				error: null,
+			};
+
+		default:
+			return state;
+	}
+};
