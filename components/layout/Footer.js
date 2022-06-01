561fef7c73542415710888e54e6f6c7d8bdbfdc2
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-01 18:29:55 +0100

    Updating the Footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className="py-1">
+			<div>
+				<p className="text-center mt-1">
+					Copyright &copy; 2022 Bookings For You | All Rights Reserved |
+					Bookings For You practice inclusivity and integrity.
+				</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
